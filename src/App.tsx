3ef3b605f19b0a7802261cb8ef8fbcdf4363a7d0
Ben Lin <blin7.webdev@gmail.com>
2023-01-26 00:59:42 -0500

    added auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import { 
+  Authenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from '@aws-amplify/ui-react';
+import "@aws-amplify/ui-react/styles.css";
+
+import { SignOut } from '@aws-amplify/ui-react/dist/types/components/Authenticator/Authenticator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {
+        ({ signOut }: { signOut: SignOut }) => (
+          <View className="App">
+            <Card>
+              <Image src={logo} className="App-logo" alt="logo" />
+              <Heading level={1}>We now have Auth!</Heading>
+              <Button onClick={signOut}>Sign Out</Button>
+            </Card>
+          </View>
+        )
+      }
+    </Authenticator>
   );
 }
 
